5db7ffbfe02ba225dcb3854f7f39113e6571d262
BetaPeche <lolopellegrino@gmail.com>
2024-02-15 16:06:56 +0100

    end of project

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
+import axios from 'axios';
+import Article from '../components/Article';
 
 const Blog = () => {
-
+    const [blogData, setblogData] = useState([])
+    const [author, setAuthor] = useState("")
     const [content, setContent] = useState("")
     const [error, setError] = useState(false)
 
-    const handleSubmit = (e) => {
+    const getData = () => {
+        axios.get("http://192.168.1.16:3004/articles").then((res) => setblogData(res.data))
+    }
+
+    useEffect(() => getData(), [])
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
         if (content.length < 140) {
             setError(true)
         } else {
+            await axios.post("http://192.168.1.16:3004/articles", {
+                author,
+                content,
+                date: Date.now(),
+            })
             setError(false)
+            setAuthor("")
+            setContent("")
+            getData()
         }
     }
 
@@ -24,12 +41,18 @@ const Blog = () => {
             <h1>Blog</h1>
 
             <form onSubmit={(e) => handleSubmit(e)}>
-                <input type="text" placeholder="Nom" />
-                <textarea placeholder='Message' onChangeCapture={(e) => setContent(e.target.value)}></textarea>
+                <input type="text" placeholder="Nom" onChange={(e) => setAuthor(e.target.value)} value={author} />
+                <textarea style={{ border: error ? "1px solid red" : "1px solid #61dafb" }} placeholder='Message' onChange={(e) => setContent(e.target.value)} value={content}></textarea>
                 {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
                 <input type="submit" value="Envoyer" />
             </form>
-            <ul></ul>
+            <ul>
+                {blogData
+                    .sort((a, b) => b.date - a.date)
+                    .map((article) => (
+                        <Article key={article.id} article={article} />
+                    ))}
+            </ul>
         </div>
     );
 };
